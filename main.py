dd57ceccc3fd8a0b8a2af539d0b4b1e1788845ee
panda <chunwang-li@outlook.com>
2025-11-25 10:25:36 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Test.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-#错误处理机制
-try:
-    x=1;
-    y="123";
-    print(x+y);
-except TypeError:
-    print("类型异常");
+import pandas as pd
+
+def read_excel(url,header=None):
+    try:
+        return pd.read_excel(url,header=header)
+    except FileNotFoundError:
+        return print('路径不存在')
+
+def main():
+    result=read_excel(
+        r"D:\OneDrive - Panda Team\文档\Temp_data_01\RPA-学习\YD-KS"
+        r"\20级计算机网络技术1班—新郑市大中专院校学生参加城乡基本医疗保险批量参保登记表.xlsx",
+        header=1
+    )
+    print(result["公民身份号码"])
+
+if __name__ == '__main__':
+    main()
